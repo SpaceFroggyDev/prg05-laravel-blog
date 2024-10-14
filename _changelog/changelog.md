98557da763bbba8881dafa1d63a727fc0a660601
Alex <alexdusselaar@gmail.com>
2024-10-14 11:10:55 +0200

    Add home

--- _changelog/changelog.md
+++ _changelog/changelog.md
@@ -1,4 +1,9 @@
 # Changelog
+
+### 14/10/2024
+* Home pagina toegevoegd
+* Home controller toegevoegd
+
 ### 09/10/2024
 * Project aangemaakt
 * Changelog aangemaakt
---
+++ resources/views/home.blade.php
@@ -0,0 +1,2 @@
+<h1>Welcome!</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque eveniet excepturi fugit, incidunt iste optio quibusdam tempore. Beatae dolor fuga iure libero, maiores modi necessitatibus nulla sapiente velit voluptas.</p>
